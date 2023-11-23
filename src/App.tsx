00767bf3a733d6e81070612f5ec4945883756800
Fanap_Dotin_80 <mh.kermanizade@gmail.com>
2023-11-23 11:05:35 +0330

    Implement the timeline canvas as the background for all 24 hours a day

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import { router } from "./routers";
 function App() {
   return (
     <div className="bg-gray-200 w-full h-screen max-h-screen flex items-center justify-center">
-      <div className="bg-navy absolute h-5/6 w-80 rounded-3xl	overflow-hidden text-white shadow-2xl">
+      <div
+        id="app"
+        className="bg-navy absolute h-5/6 w-80 rounded-3xl	overflow-hidden text-white shadow-2xl"
+      >
         <RouterProvider router={router} />
       </div>
     </div>
--- src/pages/calendar/events/EventsContainer.tsx
+++ src/pages/calendar/events/EventsContainer.tsx
@@ -1,5 +1,64 @@
+import { useEffect, useState } from "react";
+import TimeLineCanvas from "./canvas/TimeLineCanvas";
+
+export const PIXELS_PER_MINUTES = 2.5;
+export const MINUTES_IN_HOUR = 60;
+export const HOURS_IN_A_DAY = 24;
+export const TOTAL_HEIGHT =
+  PIXELS_PER_MINUTES * MINUTES_IN_HOUR * HOURS_IN_A_DAY;
+
+const returnCanvasHight = () => {
+  let appHeight = document.getElementById("app")?.clientHeight;
+  let headerHeight = document.getElementById("header")?.clientHeight;
+  let weekdaysHeight = document.getElementById("weekdays")?.clientHeight;
+  let usersHeight = document.getElementById("users")?.clientHeight;
+  let navbarHeight = document.getElementById("navbar")?.clientHeight;
+
+  if (!appHeight) return;
+  if (!headerHeight) return;
+  if (!weekdaysHeight) return;
+  if (!usersHeight) return;
+  if (!navbarHeight) return;
+
+  let canvasParentHeight =
+    appHeight - headerHeight - usersHeight - weekdaysHeight - navbarHeight;
+
+  return canvasParentHeight;
+};
+
 const EventsContainer = () => {
-  return <div className="flex-1">EventsContainer</div>;
+  const [canvasHeight, setCanvasHeight] = useState(0);
+
+  useEffect(() => {
+    let canvasHeight = returnCanvasHight();
+    if (!canvasHeight) return;
+    setCanvasHeight(canvasHeight);
+  }, [canvasHeight]);
+
+  useEffect(() => {
+    const setCanvasHeightOnResize = () => {
+      let canvasHeight = returnCanvasHight();
+      if (!canvasHeight) return;
+      setCanvasHeight(canvasHeight);
+    };
+
+    window.addEventListener("resize", setCanvasHeightOnResize);
+
+    return () => {
+      window.removeEventListener("resize", setCanvasHeightOnResize);
+    };
+  });
+
+  return (
+    <div className="flex-1">
+      <div
+        style={{ height: canvasHeight }}
+        className="relative overflow-y-scroll overflow-x-hidden"
+      >
+        <TimeLineCanvas startTime="09:00" totalHeight={TOTAL_HEIGHT} />
+      </div>
+    </div>
+  );
 };
 
 export default EventsContainer;
